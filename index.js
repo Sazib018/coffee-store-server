fb08edd0634c24a43a3f494b168e310a9e8d8344
Sazib018 <mahiburislamsazib@gmail.com>
2025-02-25 09:59:30 -0600

    add env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://mahiburislamsazib:IWlcIkzcBeH3dpjW@cluster0.rjpks.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjpks.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
